38fbe05593b2ade805192724945e167734587dae
DaGGGe <pperspectiva5@gmail.com>
2020-11-30 20:57:30 +0200

    All Fine

--- data_service.py
+++ data_service.py
@@ -1,17 +1,4 @@
-"""модуль для роботи з файлами первинних даних
-- зчитування та вивід на екран
-"""
-
-"""модуль зчитує первинні файли для обробки
-"""
-
 def get_clients():
-    """повертає список клієнтів b з файла `clients.txt`
-
-    Returns:
-        clients_list: список клієнтів
-    """
-
     with open("./data/clients.txt") as clients_file:
         from_file = clients_file.readlines()
 
@@ -21,15 +8,7 @@ def get_clients():
         clients_list.append(line_list)
 
     return clients_list
-
-
 def get_orders():
-    """повертає список накладних
-
-    Returns:
-        from_file: список накладних
-    """
-    
     with open('./data/orders.txt') as orders_file:
         from_file = orders_file.readlines()
 
@@ -45,46 +24,24 @@ def get_orders():
         line_list[4] = int(line_list[4])
         orders_list.append(line_list)
 
-    return orders_list
-
+    return orders_list  
 
 def show_clients(clients):
-    """виводить список клієнтів по заданому інтервалу кодів
-
-    Args:
-        clients (list): список клієнтів
-    """
-
     # задати інтервал виводу
-    client_code_from = input("З якого кода клієнта? ")
-    client_code_to   = input("По який кода клієнта? ")
+    client_code_from = input("З якого клієнта? ")
+    client_code_to   = input("По який клієнта? ")
 
     lines_found = 0
 
     for client in clients:
         if client_code_from <= client[0] <= client_code_to:
-            print ("код: {:5} назва: {:15} адреса: {:25}".format(*client))
+            print ("клієнт: {:5} номер: {:15} код товара: {:25} кількість: {40}".format(*client))
             lines_found += 1
 
     if lines_found == 0:
-        print("Клієнтів по Вашому запиту не знайдено") 
-
+        print("Клієнтів по Вашому запиту не знайдено")  
 
 def show_orders(orders):
-    """виводить список накладних на екран
-
-    Args:
-        orders (list): список накладних
-    """
-
     for order in orders:
-        print("код клієнта: {:3} накладна № {:4} товар: {:20} кількість: {:3} ціна: {:7}"
-            .format(order[0], order[1], order[2], order[3], order[4]))
-
-# clients = get_clients()
-# show_clients(clients)
-
-# orders= get_orders()
-# show_orders(orders)
-
-
+        print("код товару: {:3} найменування товару {:4} ціна: {:20} "
+            .format(order[0], order[1], order[2],))
